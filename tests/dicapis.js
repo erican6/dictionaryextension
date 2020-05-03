204ee872670ff73d2db05c546175b3a069915e9b
erican6 <48040678+erican6@users.noreply.github.com>
2020-05-03 15:35:55 -0400

    Add files via upload
    May 3 Update - Google non-official API complete w/output

--- tests/dicapis.js
+++ tests/dicapis.js
@@ -1,23 +1,34 @@
 $('#wordsearch').on('submit', function(e) {
-    e.preventDefault();
-    let api = 'https://api.dictionaryapi.dev/api/v1/entries/en/';
+    e.preventDefault();    
+    let api = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
     let word = $('#word').val();
-    console.log(word);
     
     let url = api + word;
-    console.log(url);
    $.get(url, function(data) {
-    console.log(data);
-    meanings = data[0]['meaning'];
-    console.log(meanings)
-    
-    index = Object.keys(meanings);
-    len = index.length;
-    for(i=0;i<len;i++) {
-        meaning = meanings[i];
-        console.log(meaning);
-    }
-   });
+        console.log(data);
+        meanings = data[0]['meanings'];
+        
+        indexm = Object.keys(meanings);
+        lenm = indexm.length;
+        defout = '';
+        for(i=0;i<lenm;i++) {
+            pos = meanings[i]['partOfSpeech'];
+            defout = defout+'<h5>Word<sup>'+i+'</sup> - '+pos+'</h5>';
 
-
+            defs = meanings[i]['definitions'];
+            indexd = Object.keys(defs);
+            lend = indexd.length;
+            defout = defout+'<ol>'
+            for(j=0;j<lend;j++) {
+                def = defs[j]['definition']
+                ex = defs[j]['example']
+                if(ex==undefined){
+                    ex = 'no example available'
+                }
+                defout = defout+'<li><b>'+def+'</b><br><i>'+ex+'</i></li>'
+            }
+            defout = defout+'</ol><hr>'
+        }
+    $('#def').html(defout);
+    });
 }); 
